48c149cef8a93316588827d9c96607f24c4b7078
Pavel <Qw3st@mail.ru>
2023-01-15 12:31:48 +0300

    добавил задачи на сесси: 24, 26, 30

---
+++ Seminar/Ex_024/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int summa (int number)
+{
+    int sum=0;
+    for (int i=1;number>=i;i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+
+
+
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(summa (num));
+
---
+++ Seminar/Ex_026/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int Bitdepth (int number)
+{
+    int count = 0;
+    int i = number;
+    while (i > 0)
+    {
+        i = i/10;
+        count +=1;
+    }
+    return count;
+
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"В числе {num} всего {(Bitdepth (num))} цифр");
