028c3c1282a8fd4ae424831f7152f5310531a6d3
Pavel <Qw3st@mail.ru>
2023-01-15 13:33:56 +0300

    добавил велосипед

--- DZ/DZ_025/Program.cs
+++ DZ/DZ_025/Program.cs
@@ -11,3 +11,15 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($" Если {num1} возвести в степень {num2} получится ({Degree (num1, num2)})");
+
+// решение через изобретение велосипеда =)
+Console.WriteLine("---------------------------");
+Console.WriteLine("Введите первое число");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+int degr = 1;
+for (int i=0; i<numb2;i++){
+    degr = degr*numb1;
+}
+Console.WriteLine($" Если {numb1} возвести в степень {numb2} получится {degr}");
