8a70b56ba120ad3ec43b9b2b30290275ed30ce8c
Pavel <Qw3st@mail.ru>
2023-01-15 13:49:43 +0300

    добавил решение через массив

--- DZ/DZ_027/Program.cs
+++ DZ/DZ_027/Program.cs
@@ -1,23 +1,22 @@
-﻿// // // решение через массив
-// Console.WriteLine("введите любое число");
-// int number = Convert.ToInt32(Console.ReadLine());
-// string massive = number.ToString();
-// Console.WriteLine($"массив {massive}");  
-// int summ = 0;
-// Console.WriteLine($"Первоначальная сумма равна {summ}");  
-// int size = massive.Length;
-// Console.WriteLine($"размер массива равен {size}");  
-// int index=0;
-// while (index < size)
-// {
-//     summ=summ + massive[index];
-//     index=index+1;
-// }
-// Console.WriteLine(summ);
-// Console.WriteLine(index);
+﻿// // решение через массив
+Console.WriteLine("Решаем через массив: введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+string massive = number.ToString();
+int sum = 0;
+int size = massive.Length;
+int index=0;
+while (index < size)
+{
+    int numb = int.Parse(Convert.ToString(massive[index]));
+    sum=sum + numb;
+    index=index+1;
+}
+Console.WriteLine($"Сумма чисел при решении через массив равна {sum}");
+
 
 // решение не через массив
 
+Console.WriteLine("---------------");
 int summ (int arg){
 int b = 10;
 int c = 1;
@@ -35,7 +34,7 @@ return summ;
 // Console.WriteLine($"a = {a}");
 // Console.WriteLine($"b = {b}");
 // Console.WriteLine($"c = {c}");
-Console.WriteLine("Введите любое число");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Решаем не через массив: введите любое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"сумма чисел = {summ (number)}");
+Console.WriteLine($"Сумма чисел при решении не через массив равна {summ (number2)}");
